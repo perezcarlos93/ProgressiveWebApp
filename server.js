d01e141a3c417907ac287fced7ee49b626d86a7b
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 14:48:16 -0800

    added useUnifiedTopology to mongoose.connect

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ const app = express();
 app.use(logger('dev'));
 
 app.use(compression());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost/budget', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+	useNewUrlParser: true,
+	useFindAndModify: false,
+	useUnifiedTopology: true,
 });
 
 // routes
 app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
