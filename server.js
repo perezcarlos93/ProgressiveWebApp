c35a90b868f42978af40667ac59c40b909ffe098
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 15:11:45 -0800

    removed local mongodb from server.js

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-	useNewUrlParser: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true,
-});
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.catch((err) => console.log(err));
 
 // routes
 app.use(require('./routes/api.js'));
