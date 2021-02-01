13b6e65c80c0c4021bedf1a6851ac4818def0870
Carlos <perez.carlos_a@yahoo.com>
2021-02-01 15:16:30 -0800

    removed MONGODB_URI from PORT const and changed to process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = process.env.MONGODB_URI || 3011;
+const PORT = process.env.PORT || 3011;
 
 const app = express();
 
